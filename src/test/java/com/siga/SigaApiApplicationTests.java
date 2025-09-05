cb345e492c88d4c4594be9eab0c3ac8278252b81
Henrique Zangaro <henrique.eng.prog@gmail.com>
2025-09-04 20:15:15 -0400

    Projeto criado

---
+++ src/test/java/com/siga/SigaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.siga;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SigaApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
